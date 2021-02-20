efaa77d2a364dbe67123947f1d16baae73d05588
kccrtv <kimcincocreative@gmail.com>
2021-02-19 16:04:53 -0800

    add event listener to js and highlight function

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Your User stories – who are your users, what do they want and why?
 Your Wireframes – sketches of major views / interfaces in your application
 Descriptions of any Unsolved problems or major hurdles you had to overcome
 Create a bronze, silver, and gold plan. This is your first project and you don't have a good way to estimate how long individual tasks will take, so it's better to set the bar low. There's no problem with under-promising and over-delivering! You can always add more features if time allows. However, if you focus on non-MVP features before your MVP is complete and run out of time, your submission will not satisfy the project requirements.
+
+References:
+https://www.w3schools.com/jsref/met_element_getattribute.asp
--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 const simon = document.querySelector('.simon');
-console.log(simon);
+const purple = document.getElementById('purple');
+const green = document.getElementById('green');
+const yellow = document.getElementById('yellow');
+const blue = document.getElementById('blue');
+const red = document.getElementById('red');
+
+simon.addEventListener('click', (event) => {
+	event.preventDefault();
+	let colorId = document.getElementById(event.target.id).getAttribute('id');
+	// console.log(colorId);
+	if (event.target.className !== 'holder piece') {
+		// console.log(document.getElementById(`${colorId}`));
+		document.getElementById(`${colorId}`).classList.add('highlight');
+	}
+});
+
+function highlight(color) {
+	color.classList.add('highlight');
+}
