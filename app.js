101963f0f5bf31d6188443f6cac80220468ec72d
kccrtv <kimcincocreative@gmail.com>
2021-02-22 12:58:18 -0800

    test get attribute id in js

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-const simon = document.querySelector('.simon');
+const wrapper = document.querySelector('.wrapper');
 // const purple = document.getElementById('purple');
 // const green = document.getElementById('green');
 // const yellow = document.getElementById('yellow');
 // const blue = document.getElementById('blue');
 // const red = document.getElementById('red');
 
-
-simon.addEventListener('click', (event) => {
+wrapper.addEventListener('click', (event) => {
 	event.preventDefault();
-	let colorId = document.getElementById(event.target.id).getAttribute('id');
-	if (event.target.className !== 'holder') {
-		document.getElementById(`${colorId}`).classList.add('highlight');
-		setTimeout(function () {
-			document.getElementById(`${colorId}`).classList.remove('highlight');
-		}, 1000);
+
+	let colorId = document.getElementById(event.target.id);
+	if (colorId !== null) {
+		console.log(colorId.getAttribute('id'));
 	}
+	// .getAttribute('id');
+	// document.getElementById(`${colorId}`).classList.add('highlight');
+	// console.log(colorId);
 });
 
-const levelUp (()=> {
-    const level = document.querySelector('.header');
-    let currentLevel = 0;
-    level.textContent = `Level ${currentLevel}`;
-})
+// if (event.target.className !== 'holder') {
+// 	let colorId = document.getElementById(event.target.id).getAttribute('id');
+// 	document.getElementById(`${colorId}`).classList.add('highlight');
+// 	setTimeout(function () {
+// 		document.getElementById(`${colorId}`).classList.remove('highlight');
+// 	}, 1000);
+// }
+// });
+
+// const levelUp (()=> {
+//     const level = document.querySelector('.header');
+//     let currentLevel = 0;
+//     level.textContent = `Level ${currentLevel}`;
+// })
