7404e59bc1e29bef0d28bf4b175af895db4e2015
kccrtv <kimcincocreative@gmail.com>
2021-02-23 13:28:06 -0800

    refactor js and complete bronze mvp items

--- README.md
+++ README.md
@@ -32,4 +32,4 @@ Project 1 for General Assembly's remote Software Engineering Immersive program
 
 ### References:
 
-> https://www.w3schools.com/jsref/met_element_getattribute.asp > https://www.w3schools.com/jsref/met_win_settimeout.asp > https://www.youtube.com/watch?v=br-0i3U1VCA > https://www.youtube.com/watch?v=EashgVqboWo&feature=emb_title > https://www.youtube.com/watch?v=0Gr1XSyxZy0 > https://www.udemy.com/course/the-complete-web-development-bootcamp/ > https://css-tricks.com/almanac/properties/f/filter/
+> https://www.w3schools.com/jsref/met_element_getattribute.asp > https://www.w3schools.com/jsref/met_win_settimeout.asp > https://www.youtube.com/watch?v=br-0i3U1VCA > https://www.youtube.com/watch?v=EashgVqboWo&feature=emb_title > https://www.youtube.com/watch?v=0Gr1XSyxZy0 > https://www.udemy.com/course/the-complete-web-development-bootcamp/ > https://css-tricks.com/almanac/properties/f/filter/>https://www.w3schools.com/jsref/prop_style_filter.asp
--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/********************** REFACTORING! **********************/
 let gameSequence = [];
 let userMoves = [];
 let game = false;
@@ -7,11 +6,11 @@ const level = document.querySelector('.header');
 const wrapper = document.querySelector('.wrapper');
 const pieceColors = ['green', 'yellow', 'red', 'blue', 'purple'];
 
-//starting the game
 window.document.addEventListener('keypress', () => {
 	if (!game) {
 		simonMoves();
 		level.textContent = `Level ${currentLevel}`;
+		document.querySelector('.restart').style.display = 'none';
 		game = true;
 	}
 });
@@ -20,7 +19,6 @@ wrapper.addEventListener('click', (event) => {
 	event.preventDefault();
 	let user = document.getElementById(event.target.id).getAttribute('id');
 	userMoves.push(user);
-	console.log(`User pattern: ${userMoves}`);
 	validate(userMoves.length - 1);
 	highlight(user);
 });
@@ -34,11 +32,11 @@ function validate(currentLevel) {
 		}
 	} else {
 		level.textContent = `Game Over!`;
+		document.querySelector('.restart').style.display = 'inline-block';
 		reset();
 	}
 }
 
-//simon's turn
 function simonMoves() {
 	userMoves = [];
 	currentLevel++;
@@ -51,12 +49,11 @@ function simonMoves() {
 
 	gameSequence.push(simon);
 	console.log(`game sequence: ${gameSequence}`);
-	// highlight(simon);
-
+	document.querySelector('.wrapper').filter = 'brightness(2)';
+	simonPiece.classList.add('highlight');
 	setTimeout(() => {
-		simonPiece.classList.add('highlight');
-		simonPiece.classList.remove('pressed');
-	}, 400);
+		simonPiece.classList.remove('highlight');
+	}, 300);
 
 	//fade in/out/in animation + play sound
 }
@@ -65,7 +62,7 @@ function highlight(currentPiece) {
 	document.getElementById(`${currentPiece}`).classList.add('highlight');
 	setTimeout(() => {
 		document.getElementById(`${currentPiece}`).classList.remove('highlight');
-	}, 200);
+	}, 100);
 }
 
 function reset() {
@@ -73,90 +70,3 @@ function reset() {
 	gameSequence = [];
 	game = false;
 }
-
-/********************** FIRST DRAFT **********************/
-// const wrapper = document.querySelector('.wrapper');
-// const pieceColors = ['green', 'yellow', 'red', 'blue', 'purple'];
-// let userMoves = [];
-// let startGame = false;
-// let currentLevel = 0;
-// const level = document.querySelector('.header');
-
-// window.document.addEventListener('keypress', () => {
-// 	if (!startGame) {
-// 		simonMoves();
-// 	}
-// });
-
-// const simonMoves = () => {
-// 	return game();
-// };
-
-// wrapper.addEventListener('click', (event) => {
-// 	event.preventDefault();
-// 	let colorId = document.getElementById(event.target.id);
-// 	if (colorId !== null) {
-// 		colorId = colorId.getAttribute('id');
-// 		document.getElementById(`${colorId}`).classList.add('highlight');
-// 		setTimeout(function () {
-// 			document.getElementById(`${colorId}`).classList.remove('highlight');
-// 		}, 1000);
-// 		userMoves.push(`${colorId}`);
-// 		console.log(`Player's moves: ${userMoves}`);
-// 	}
-// 	return gameSequence;
-// });
-
-// /********************** PSEUDOCODE STEPS**********************/
-// //create a random sequence
-// // const randomNumber = () => {
-// // 	let num = Math.floor(Math.random() * 5);
-// // 	gameSequence.push(num);
-// // };
-
-// let gameSequence = [];
-
-// const randomSimon = () => {
-// 	let currentRandomNumber = randomNumber();
-// 	let simonsColors = pieceColors[currentRandomNumber];
-// 	return gameSequence.push(simonsColors);
-// };
-
-// //display current level
-
-// const levelUp = () => {
-// 	currentLevel++;
-// 	return (level.textContent = `Level ${currentLevel}`);
-// };
-
-// //display Simon's colors at the start of each level and flash corresponding color piece
-// let piece = gameSequence[0];
-// const game = () => {
-// 	randomSimon();
-// 	piece = gameSequence[gameSequence.length - 1];
-// 	let pieceHighlight = document.getElementById(`${piece}`);
-// 	pieceHighlight.classList.add('highlight');
-// 	setTimeout(function () {
-// 		pieceHighlight.classList.remove('highlight');
-// 	}, 1000);
-// 	console.log(`Simon: ${gameSequence}`);
-// 	levelUp();
-// 	checkAnswer(currentLevel);
-// 	return gameSequence;
-// };
-
-// //wait for user's input and cross-check if the input matches the game's input
-
-// //if matches, proceed to next level
-
-// //else, game over, and game resets
-
-// //inform user if game is over
-// function checkAnswer(currentLevel) {
-// 	console.log(currentLevel);
-// 	if (userMoves === gameSequence) {
-// 		console.log('success');
-// 	} else {
-// 		console.log('wrong');
-// 	}
-// }
