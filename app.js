e432572bc98376a6e041401c535814feaa4d637e
kccrtv <kimcincocreative@gmail.com>
2021-02-22 16:21:49 -0800

    begin writing function on checking user input against simon's

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const wrapper = document.querySelector('.wrapper');
 const pieceColors = ['green', 'yellow', 'red', 'blue', 'purple'];
 let userMoves = [];
+let startGame = false;
+let currentLevel = 0;
+const level = document.querySelector('.header');
+
+window.document.addEventListener('keypress', () => {
+	if (!startGame) {
+		simonMoves();
+	}
+});
+const simonMoves = () => {
+	return game();
+};
 
 wrapper.addEventListener('click', (event) => {
 	event.preventDefault();
@@ -14,6 +26,7 @@ wrapper.addEventListener('click', (event) => {
 		userMoves.push(`${colorId}`);
 		console.log(`Player's moves: ${userMoves}`);
 	}
+	return gameSequence;
 });
 
 /********************** PSEUDOCODE STEPS**********************/
@@ -24,16 +37,15 @@ const randomNumber = () => {
 
 let gameSequence = [];
 
-const simonMoves = () => {
+const randomSimon = () => {
 	let currentRandomNumber = randomNumber();
 	let simonsColors = pieceColors[currentRandomNumber];
 	return gameSequence.push(simonsColors);
 };
 
 //display current level
+
 const levelUp = () => {
-	const level = document.querySelector('.header');
-	let currentLevel = 0;
 	currentLevel++;
 	return (level.textContent = `Level ${currentLevel}`);
 };
@@ -41,21 +53,18 @@ const levelUp = () => {
 //display Simon's colors at the start of each level and flash corresponding color piece
 let piece = gameSequence[0];
 const game = () => {
-	simonMoves();
-	levelUp();
+	randomSimon();
 	piece = gameSequence[gameSequence.length - 1];
 	let pieceHighlight = document.getElementById(`${piece}`);
 	pieceHighlight.classList.add('highlight');
 	setTimeout(function () {
 		pieceHighlight.classList.remove('highlight');
 	}, 1000);
-	console.log(`Simon's moves: ${gameSequence}`);
+	console.log(`Simon: ${gameSequence}`);
+	levelUp();
+	checkAnswer(currentLevel);
 	return gameSequence;
 };
-
-game();
-
-//store the input of the player
 
 //wait for user's input and cross-check if the input matches the game's input
 
@@ -64,3 +73,11 @@ game();
 //else, game over, and game resets
 
 //inform user if game is over
+function checkAnswer(currentLevel) {
+	console.log(currentLevel);
+	if (userMoves === gameSequence) {
+		console.log('success');
+	} else {
+		console.log('wrong');
+	}
+}
