3810e7aa9e820d23e1f9c0c3b9874515311f9cc1
kccrtv <kimcincocreative@gmail.com>
2021-02-24 17:33:41 -0800

    debug loops

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const restart = document.querySelector('.restart');
 const wrapper = document.querySelector('.wrapper');
 const pieceList = document.querySelectorAll('img');
 const pieceColors = ['green', 'yellow', 'red', 'blue', 'purple'];
+const numberOfPieces = document.querySelectorAll(`.piece`).length;
+const pieceColor = document.querySelectorAll('.piece');
 
 window.document.addEventListener('keypress', () => {
 	if (!game) {
@@ -18,88 +20,22 @@ window.document.addEventListener('keypress', () => {
 		level.textContent = `Level ${currentLevel}`;
 		restart.style.display = 'none';
 		game = true;
+		userTurn = false;
 	}
 });
-
-const numberOfPieces = document.querySelectorAll(`.piece`).length;
-const pieceColor = document.querySelectorAll('.piece');
 
 for (let i = 0; i < numberOfPieces; i++) {
 	pieceColor[i].addEventListener('click', (event) => {
 		event.preventDefault();
 		let pieceColorId = pieceColor[i].dataset.id;
 		userTurn = true;
-		console.log(pieceColorId);
+		// console.log(pieceColorId);
 		animation(parseInt(pieceColorId));
-		// animateCSS(pieceColor[pieceColorId]);
 		userSequence.push(pieceColor[i].id);
 		validate(userSequence - 1);
 		playSound(pieceColor[i].id);
 		score.style.display = 'inline';
 	});
-
-	// 	function animateCSS(element, animation, prefix = 'animate__') {
-	// 		let animationStyle = '';
-
-	// 		new Promise((resolve, reject) => {
-	// 			if (element === pieceColor[0]) {
-	// 				animationStyle = 'shakeY';
-	// 				pieceColor[0].classList.add(
-	// 					`${prefix}animated`,
-	// 					`${prefix}${animation}`
-	// 				);
-	// 				setTimeout(() => {
-	// 					pieceColor[0].classList.remove(
-	// 						`${prefix}animated`,
-	// 						`${prefix}${animation}`
-	// 					);
-	// 				}, 100);
-	// 			}
-
-	// 			// switch (element[key]) {
-	// 			// 	case 0:
-	// 			// 		pieceColor[0].add('animate__animated', 'animate__shakeY');
-
-	// 			// 		// animateCSS(green, 'shakeY');
-	// 			// 		// green.addEventListener('animationend', () => {
-	// 			// 		// 	green.classList.remove('animate__animated', 'animate__shakeY');
-	// 			// 		// });
-	// 			// 		break;
-	// 			// 	case 1:
-	// 			// 		// const yellow = document.querySelector('#yellow');
-	// 			// 		yellow.classList.add('animate__animated', 'animate__bounce');
-	// 			// 		yellow.addEventListener('animationend', () => {
-	// 			// 			yellow.classList.remove('animate__animated', 'animate__bounce');
-	// 			// 		});
-	// 			// 		break;
-	// 			// 	case 3:
-	// 			// 		// const red = document.querySelector('#red');
-	// 			// 		red.classList.add('animate__animated', 'animate__swing');
-	// 			// 		red.addEventListener('animationend', () => {
-	// 			// 			red.classList.remove('animate__animated', 'animate__swing');
-	// 			// 		});
-	// 			// 		break;
-	// 			// 	case 4:
-	// 			// 		// const purple = document.querySelector('#purple');
-	// 			// 		purple.classList.add('animate__animated', 'animate__bounceIn');
-	// 			// 		purple.addEventListener('animationend', () => {
-	// 			// 			purple.classList.remove('animate__animated', 'animate__bounceIn');
-	// 			// 		});
-	// 			// 		break;
-	// 			// 	case 5:
-	// 			// 		// const blue = document.querySelector('#blue');
-	// 			// 		blue.classList.add('animate__animated', 'animate__shakeY');
-	// 			// 		blue.addEventListener('animationend', () => {
-	// 			// 			blue.classList.remove('animate__animated', 'animate__shakeY');
-	// 			// 		});
-	// 			// 		break;
-	// 			// 	default:
-	// 			// 		console.log(`Switch Case: ${key}`);
-	// 			// 		break;
-	// 			// }
-	// 		});
-	// 	}
-	// }
 
 	// function userMoves() {
 	// 	wrapper.addEventListener('click', (event) => {
@@ -150,7 +86,7 @@ for (let i = 0; i < numberOfPieces; i++) {
 	}
 
 	function showPattern(array) {
-		let interval = 1500;
+		let interval = 900;
 		let promise = Promise.resolve();
 		array.forEach((array) => {
 			promise = promise.then(() => {
@@ -175,9 +111,7 @@ for (let i = 0; i < numberOfPieces; i++) {
 		let piece = gameSequence[0];
 		let simon = pieceColors[currentRandomNumber];
 		// let simonPiece = document.querySelector(`#${simon}`).dataset.id;
-		// console.log(`Simon Piece: ${simonPiece}`);
-		// console.log(simonPiece);
-		// animation(simonPiece);
+		// animation(parseInt(simonPiece));
 		piece = gameSequence[gameSequence.length - 1];
 		gameSequence.push(simon);
 		showPattern(gameSequence);
@@ -207,88 +141,72 @@ for (let i = 0; i < numberOfPieces; i++) {
 		score.style.display = 'none';
 	}
 
-	/**
-	 * green - animate__shakeY
-	 * yellow - bounce
-	 * red - swing
-	 * purple - bounceIn
-	 * blue - animate__shakeY
-	 */
-
-	// const animateCSS = (element, animation, prefix = 'animate__') =>
-	// 	// We create a Promise and return it
-	// 	new Promise((resolve, reject) => {
-	// 		const animationName = `${prefix}${animation}`;
-	// 		// const green = document.querySelector('#green');
-	// 		// const yellow = document.querySelector('#yellow');
-	// 		// const red = document.querySelector('#red');
-	// 		// const purple = document.querySelector('#purple');
-	// 		// const blue = document.querySelector('#blue');
-
-	// 		node.classList.add(`${prefix}animated`, animationName);
-
-	// 		// When the animation ends, we clean the classes and resolve the Promise
-	// 		function handleAnimationEnd(event) {
-	// 			event.stopPropagation();
-	// 			node.classList.remove(`${prefix}animated`, animationName);
-	// 			resolve('Animation ended');
-	// 		}
-
-	// 		node.addEventListener('animationend', handleAnimationEnd, { once: true });
-	// 	});
-
 	function animation(key) {
-		// let img = document.querySelectorAll('img')[key].dataset.id;
 		switch (key) {
 			case 0:
-				const green = pieceColor[0];
-				green.classList.add('animate__animated', 'animate__shakeY');
-				green.addEventListener('animationend', () => {
-					green.classList.remove('animate__animated', 'animate__shakeY');
-				});
+				console.log(pieceColor[0]);
+				pieceColor[0].classList.add('animate__animated', 'animate__shakeY');
+				setTimeout(() => {
+					pieceColor[0].classList.remove(
+						'animate__animated',
+						'animate__shakeY'
+					);
+				}, 100);
 				break;
+
 			case 1:
-				const yellow = pieceColor[1];
-				yellow.classList.add('animate__animated', 'animate__bounce');
-				yellow.addEventListener('animationend', () => {
-					yellow.classList.remove('animate__animated', 'animate__bounce');
-				});
+				console.log(pieceColor[1]);
+				pieceColor[1].classList.add('animate__animated', 'animate__bounce');
+				setTimeout(() => {
+					pieceColor[1].classList.remove(
+						'animate__animated',
+						'animate__bounce'
+					);
+				}, 100);
 				break;
+
+			case 2:
+				console.log(pieceColor[2]);
+				pieceColor[2].classList.add('animate__animated', 'animate__swing');
+				setTimeout(() => {
+					pieceColor[2].classList.remove('animate__animated', 'animate__swing');
+				}, 100);
+				break;
+
 			case 3:
-				const red = pieceColor[3];
-				red.classList.add('animate__animated', 'animate__swing');
-				red.addEventListener('animationend', () => {
-					red.classList.remove('animate__animated', 'animate__swing');
-				});
+				console.log(pieceColor[3]);
+				pieceColor[3].classList.add('animate__animated', 'animate__bounceIn');
+				setTimeout(() => {
+					pieceColor[3].classList.remove(
+						'animate__animated',
+						'animate__bounceIn'
+					);
+				}, 100);
 				break;
+
 			case 4:
-				const purple = pieceColor[4];
-				purple.classList.add('animate__animated', 'animate__bounceIn');
-				purple.addEventListener('animationend', () => {
-					purple.classList.remove('animate__animated', 'animate__bounceIn');
-				});
+				console.log(pieceColor[4]);
+				pieceColor[4].classList.add('animate__animated', 'animate__shakeY');
+				setTimeout(() => {
+					pieceColor[4].classList.remove(
+						'animate__animated',
+						'animate__shakeY'
+					);
+				}, 100);
 				break;
-			case 5:
-				const blue = pieceColor[5];
-				blue.classList.add('animate__animated', 'animate__shakeY');
-				blue.addEventListener('animationend', () => {
-					blue.classList.remove('animate__animated', 'animate__shakeY');
-				});
-				break;
+
 			default:
 				return `Switch Case: ${key}`;
-				break;
 		}
 	}
-
-	// animateCSS()
-	/**
-	 * nodelist:
-	 * 0 green data-id 0
-	 * 1 yellow data-id 1
-	 * 2 holder data-id n/a
-	 * 3 red data-id 3
-	 * 4 purple data-id 4
-	 * 5 blue data-id 5
-	 */
 }
+
+/**
+ * nodelist:
+ * 0 green data-id 0
+ * 1 yellow data-id 1
+ * 2 holder data-id n/a
+ * 3 red data-id 3
+ * 4 purple data-id 4
+ * 5 blue data-id 5
+ */
